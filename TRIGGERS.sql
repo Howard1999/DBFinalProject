749f1539d4c944873b036e5bd2d66391c416e553
Howard <a82823553@gmail.com>
2020-06-20 16:24:54 +0800

    fix login time trigger

--- TRIGGERS.sql
+++ TRIGGERS.sql
@@ -5,7 +5,7 @@ create trigger login_time_update before update on user
 		begin
 			if !(new.session_id <=> old.session_id) and !(new.session_id <=> NULL)
 			then
-				set new.last_login_time=new.login_time;
+				set new.last_login_time=old.login_time;
 				set new.login_time=current_timestamp;
 			end if;
 		end&&
