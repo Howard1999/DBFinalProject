16d55141398c7bb04595270bc46f4f734d6ac6b7
cmaevtaelovr <55286813+cmaevtaelovr@users.noreply.github.com>
2020-06-25 19:21:12 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+	$severname = "127.0.0.1";
+	$username = "team1";
+	$password = "DB338HKkvRVOZzb";
+	$dbname = "team1";
+	
+	$conn = new mysqli($servername,$username,$password,$dbname);
+	
+	if(!$conn->set_charset("utf8"))
+	{
+		printf("Error loading character set utf8: %s\n",$conn->error);
+		exit();
+	}
+	
+	if($conn->connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$username=$_POST['username'];
+	$account=$_POST['account'];
+	$password=$_POST['password'];
+	
+	$message = "你輸入的資訊為:\\n使用者名稱:$username\\n帳號:$account\\n密碼:$password , 即將導向登入頁面";
+    echo "<script type='text/javascript'>alert('$message');</script>";
+	mysql_query("insert into Register values('$username','$account','$password')");
+
+	//done
+?>
