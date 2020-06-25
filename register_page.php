edc023790f3122cc57999f0b14dd76c0ef0fa840
cmaevtaelovr <55286813+cmaevtaelovr@users.noreply.github.com>
2020-06-25 20:59:48 +0800

    Update register_page.php

--- register_page.php
+++ register_page.php
@@ -1,24 +1,26 @@
+<!doctype html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title> 註冊系統 </title>
 </head>
 <body>
-	<form method="post" action = "register.php">
+	<form method="post" action="register.php" >
 	<table border="1">
 　	<tr>
 　	<td>使用者名稱</td>
-	<td><input type="text" placeholder="user_name" name="user_name"/></td>
+	<td><input type="text" name="user_name"/></td>
 	</tr>
 	<tr>
 	<td>帳號</td>
-	<td><input type="text" placeholder="account" name="account"/></td>
+	<td><input type="text" name="account"/></td>
 	</tr>
 	<tr>
 	<td>密碼</td>
-	<td><input type="text" placeholder="password" name="password"/></td>
+	<td><input type="password" name="password"/></td>
 	</tr>
 	</table>
-	<input id="submit_button" type="submit">
+	<input type ="submit" value="註冊">
 	</form>
 </body>
 </html>
