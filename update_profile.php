8adc0f19e3b290bd3347cc9a15a5e3de898515b2
ninjat6 <67402459+ninjat6@users.noreply.github.com>
2020-06-26 22:29:49 +0800

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -1,1 +1,38 @@
-hi
+<form action="profile_page.php" method="post">
+  <input type="text" name="user_name" placeholder="輸入使用者名稱："/><br/> 
+  <input type="text" name="password" placeholder="輸入新密碼："/><br/> 
+  
+  <input type="submit" name="update" value="更改"/>
+</form>
+
+<?php
+session_start();
+$servername = "127.0.0.1";
+$username = "team1";
+$password = "DB338HKkvRVOZzb";
+$dbname = "team1";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if (!$conn->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $conn->error);
+    exit();
+}
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST['update'])){
+    $account = $_POST['account'];
+  
+    $query = "UPDATE 'users' SET user_name='$_POST[user_name]',password='$_POST[password]' WHERE session_id = $_SESSION["session_id"]";
+    $query_run = mysqli_query($conn,$query)
+      
+    if($query_run){
+      echo '<script type="text/javascript"> alert("Data Updated") </script>';
+    }else{
+      echo '<script type="text/javascript"> alert("Data No Updated") </script>';
+    }
+}
+?>
