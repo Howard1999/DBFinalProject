7680070cddbe0146c4a11741ede1d901b7736a00
ninjat6 <67402459+ninjat6@users.noreply.github.com>
2020-06-26 17:36:32 +0800

    Update profile_page.php

--- profile_page.php
+++ profile_page.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	$account = $_SESSION['account'];
 	$servername = "127.0.0.1";
 	$username = "team1";
 	$password = "DB338HKkvRVOZzb";
@@ -15,17 +16,29 @@
 		die("Connection failed: " . $conn->connect_error);
 	}
 
-	$query = $db->query("SELECT * from user");
+	
+	$sql="SELECT * FROM users WHERE account = '$account'";
+	
 
-	while($r = $query->fetch()) {
+if ($result = mysqli_query($conn,$sql))
+{
+	while($r = mysqli_fetch_row($result)) {
 	    echo '<div class="col-sm-6 col-md-6 col-lg-3 membersGrid">';
-	    echo  $r['user_name'], '<br>';
-		echo  $r['account'], '<br>';
-		echo  $r['password'], '<br>';
-	    echo  $r['user_authority'], '<br>';
-		echo  $r['board_name'], '<br>';
-	    echo '<a class="viewProfile" href="profile_page.php?id=' . $r['id']. '"><button>View Profile</button></a>';
+	    
+		printf ("個人資料");
+		echo '<br>';
+		printf ("使用者名稱 : %s",$r[0]);
+		echo '<br>';
+		printf ("帳號 : %s",$r[1]);
+		echo '<br>';
+		printf ("密碼 : %s",$r[2]);
+		echo '<br>';
+		printf ("使用者權限 : %s",$r[3]);
+		echo '<br>';
+		printf ("擁有版名 : %s",$r[6]);
+        echo "<br>";
 	    echo '</div>';
 	}
-
+	mysqli_free_result($result);
+}
 ?>
