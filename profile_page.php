c238f5cda28e045eb4646ee5475685d29e3096d8
ninjat6 <67402459+ninjat6@users.noreply.github.com>
2020-06-26 21:49:49 +0800

    Update profile_page.php

--- profile_page.php
+++ profile_page.php
@@ -1,6 +1,5 @@
 <?php
 	session_start();
-	$account = $_SESSION['account'];
 	$servername = "127.0.0.1";
 	$username = "team1";
 	$password = "DB338HKkvRVOZzb";
@@ -17,7 +16,7 @@
 	}
 
 	
-	$sql="SELECT * FROM users WHERE account = '$account'";
+	$sql="SELECT * FROM users WHERE session_id = $_SESSION["session_id"]";
 	
 
 if ($result = mysqli_query($conn,$sql))
@@ -36,7 +35,16 @@ if ($result = mysqli_query($conn,$sql))
 		printf ("使用者權限 : %s",$r[3]);
 		echo '<br>';
 		printf ("擁有版名 : %s",$r[6]);
-        echo "<br>";
+        	echo "<br>";
+		printf ("登入時間 : %s",$r[4]);
+        	echo "<br>";
+		printf ("最後登入時間 : %s",$r[5]);
+        	echo "<br>";
+		printf ("這次登入的ip : %s",$r[7]);
+        	echo "<br>";
+		printf ("上次登入的ip : %s",$r[8]);
+        	echo "<br>";
+		
 	    echo '</div>';
 	}
 	mysqli_free_result($result);
