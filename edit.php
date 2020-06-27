d4458561e9285920179c5069d9907eaae7f90169
Howard <a82823553@gmail.com>
2020-06-27 13:26:35 +0800

    edit and post add string length restrict

--- edit.php
+++ edit.php
@@ -26,6 +26,7 @@
 			die();
 		}
 	}
+	
 	// check login
 	if(isset($_SESSION['session_id'])){
 		$result=$conn->query('select account from user where session_id="'.$_SESSION['session_id'].'"');
@@ -47,67 +48,117 @@
 		$building_ID = mysqli_real_escape_string($conn,$_POST['building_ID']);
 		$article_ID = mysqli_real_escape_string($conn,$_POST['article_ID']);
 	}
-	if(isset($conn,$_POST['title']))
+	// check string
+	$string_check = true;
+	if(isset($conn,$_POST['title'])){
 		$title = mysqli_real_escape_string($conn,$_POST['title']);
+		if(strlen($title)==0){
+			?>
+				<script type="text/javascript">
+				alert("標題不能為空");
+			<?php
+				echo 'window.location.href="edit_page.php?building_ID='.$building_ID.'&article_ID='.$article_ID.'";';
+			?>
+				</script>
+			<?php
+			$string_check = false;
+		}
+		else if(strlen($title)>50){
+			?>
+				<script type="text/javascript">
+				alert("標題長度需在50以下");
+			<?php
+				echo 'window.location.href="edit_page.php?building_ID='.$building_ID.'&article_ID='.$article_ID.'";';
+			?>
+				</script>
+			<?php
+			$string_check = false;
+		}
+	}
 	$content = mysqli_real_escape_string($conn,$_POST['content']);
-	
-	$should_rollback = false;
-	$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-	$success = true;
-	// get article owner's account
-	$result = $conn->query('select account from article where article_ID='.$article_ID);
-	if($row = $result->fetch_row()){
-		$owner_account = $row[0];
-	}
-	else{
-		redirect();
-	}
-	$result->close();
-	// check if user is owner
-	if($account!=$owner_account)redirect();
-	// if post data has title get first article_ID of building and check is equal
-	if(isset($title)){
-		$is_first = false;
-		$result = $conn->query('select min(article_ID) from article where building_ID='.$building_ID);
-		if($row = $result->fetch_row()){
-			$first_article_ID = $row[0];
-			if($first_article_ID==$article_ID){
-				$is_first = true;
-			}
-		}
-		$result->close();
-		if(!$is_first){
-			redirect();
-		}
-		if(!$conn->query('update article_building set title="'.$title.'" where building_ID='.$building_ID)){
-			$should_rollback = true;
-		}
-	}
-	if(!$conn->query('update article set content="'.$content.'" where article_ID='.$article_ID)){
-		$should_rollback = true;
-	}
-	
-	if($should_rollback){
-		$conn->rollback();
+	if(strlen($content)==0){
 		?>
 			<script type="text/javascript">
-			alert("更新失敗，請稍後再試");
-			
-			<?php
-			if(isset($_SESSION['last view board'])&&isset($_SESSION['last view page'])){
-				echo "window.location.href = '/DBFinalProject/board.php?board_name=".$_SESSION['last view board']."&page=".$_SESSION['last view page']."';";
-			}
-			else{
-				echo '"window.location.href ="index.php;"';
-			}
-			?>
+			alert("內文不能為空");
+		<?php
+			echo 'window.location.href="edit_page.php?building_ID='.$building_ID.'&article_ID='.$article_ID.'";';
+		?>
 			</script>
 		<?php
+		$string_check = false;
 	}
-	else{
-		$conn->commit();
-		header("Location: /DBFinalProject/article_building.php?building_ID=".$building_ID);
-		die();
+	else if(strlen($content)>10000){
+		?>
+			<script type="text/javascript">
+			alert("內文限制10000字以下");
+		<?php
+			echo 'window.location.href="edit_page.php?building_ID='.$building_ID.'&article_ID='.$article_ID.'";';
+		?>
+			</script>
+		<?php
+		$string_check = false;
 	}
+	
+	if($string_check){
+		$should_rollback = false;
+		$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
+		$success = true;
+		// get article owner's account
+		$result = $conn->query('select account from article where article_ID='.$article_ID);
+		if($row = $result->fetch_row()){
+			$owner_account = $row[0];
+		}
+		else{
+			redirect();
+		}
+		$result->close();
+		// check if user is owner
+		if($account!=$owner_account)redirect();
+		// if post data has title get first article_ID of building and check is equal
+		if(isset($title)){
+			$is_first = false;
+			$result = $conn->query('select min(article_ID) from article where building_ID='.$building_ID);
+			if($row = $result->fetch_row()){
+				$first_article_ID = $row[0];
+				if($first_article_ID==$article_ID){
+					$is_first = true;
+				}
+			}
+			$result->close();
+			if(!$is_first){
+				redirect();
+			}
+			if(!$conn->query('update article_building set title="'.$title.'" where building_ID='.$building_ID)){
+				$should_rollback = true;
+			}
+		}
+		if(!$conn->query('update article set content="'.$content.'" where article_ID='.$article_ID)){
+			$should_rollback = true;
+		}
+		
+		if($should_rollback){
+			$conn->rollback();
+			?>
+				<script type="text/javascript">
+				alert("更新失敗，請稍後再試");
+				
+				<?php
+				if(isset($_SESSION['last view board'])&&isset($_SESSION['last view page'])){
+					echo "window.location.href = '/DBFinalProject/board.php?board_name=".$_SESSION['last view board']."&page=".$_SESSION['last view page']."';";
+				}
+				else{
+					echo '"window.location.href ="index.php;"';
+				}
+				?>
+				</script>
+			<?php
+		}
+		else{
+			$conn->commit();
+			header("Location: /DBFinalProject/article_building.php?building_ID=".$building_ID);
+			die();
+		}
+	}
+	
 	$conn->close();
 ?>
--- post.php
+++ post.php
@@ -40,54 +40,92 @@
 		header("Location: /DBFinalProject/login_page.php?err_msg=post_without_login");
 		die();
 	}
-	$success = false;
-	$content = mysqli_real_escape_string($conn,$_POST['content']);
 	if((isset($_POST['board_name'])&&isset($_POST['building_ID']))||!(isset($_POST['board_name'])||isset($_POST['building_ID']))){
 		redirect();
 	}
 	
-	// post on board
-	else if(isset($_POST['board_name'])){
-		$board_name = mysqli_real_escape_string($conn,$_POST['board_name']);
+	$success = false;
+	$string_check = true;
+
+	if(isset($_POST['title'])){
 		$title = mysqli_real_escape_string($conn,$_POST['title']);
-		
-		$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
-		$success = false;
-		if($conn->query('insert into article_building values(default,"'.$board_name.'","'.$account.'","'.$title.'",current_timestamp)')){
-			$building_ID = $conn->insert_id;
+		if(strlen($title)==0){
+			$string_check = false;
+			$err_msg = "標題不能為空";
+		}
+		else if(strlen($title)>50){
+			$err_msg = "標題長度需在50以下";
+			$string_check = false;
+		}
+	}
+	$content = mysqli_real_escape_string($conn,$_POST['content']);
+	if(strlen($content)==10){
+		$err_msg = "內文不能為空";
+		$string_check = false;
+	}
+	else if(strlen($content)>10000){
+		$err_msg = "內文限制10000字以下";
+		$string_check = false;
+	}
+	
+	if($string_check){
+		// post on board
+		if(isset($_POST['board_name'])){
+			$board_name = mysqli_real_escape_string($conn,$_POST['board_name']);
+
+			$conn->begin_transaction(MYSQLI_TRANS_START_READ_WRITE);
+			if($conn->query('insert into article_building values(default,"'.$board_name.'","'.$account.'","'.$title.'",current_timestamp)')){
+				$building_ID = $conn->insert_id;
+				if($conn->query('insert into article values(default,'.$building_ID.',"'.$account.'","'.$content.'",current_timestamp)')){
+					$success = true;
+				}
+			}
+			if(!$success){
+				$conn->rollback();
+			}
+			$conn->commit();
+		}
+		// post on article building
+		else if(isset($_POST['building_ID'])){
+			$building_ID = mysqli_real_escape_string($conn,$_POST['building_ID']);
 			if($conn->query('insert into article values(default,'.$building_ID.',"'.$account.'","'.$content.'",current_timestamp)')){
 				$success = true;
 			}
 		}
-		if(!$success){
-			$conn->rollback();
+		$conn->close();
+		if($success){
+			header("Location: /DBFinalProject/article_building.php?building_ID=".$building_ID);
+			die();
 		}
-		$conn->commit();
-	}
-	// post on article building
-	else if(isset($_POST['building_ID'])){
-		$building_ID = mysqli_real_escape_string($conn,$_POST['building_ID']);
-		if($conn->query('insert into article values(default,'.$building_ID.',"'.$account.'","'.$content.'",current_timestamp)')){
-			$success = true;
+		else{
+			?>
+				<script type="text/javascript">
+				alert("PO文失敗，請稍後再試");
+				<?php
+					if(isset($_POST['board_name'])){
+						echo 'window.location.href ="post_page.php?board_name='.$board_name.'";';
+					}
+					else if(isset($_POST['building_ID'])){
+						echo 'window.location.href ="post_page.php?building_ID='.$building_ID.'";';
+					}
+				?>
+				</script>
+			<?php
 		}
-	}
-	$conn->close();
-	if($success){
-		header("Location: /DBFinalProject/article_building.php?building_ID=".$building_ID);
-		die();
 	}
 	else{
 		?>
 			<script type="text/javascript">
-			alert("PO文失敗，請稍後再試");
-			
 			<?php
-			if(isset($_SESSION['last view board'])&&isset($_SESSION['last view page'])){
-				echo "window.location.href = '/DBFinalProject/board.php?board_name=".$_SESSION['last view board']."&page=".$_SESSION['last view page']."';";
-			}
-			else{
-				echo '"window.location.href ="index.php;"';
-			}
+				echo 'alert("'.$err_msg.'");';
+				if(isset($_POST['board_name'])){
+					$board_name = mysqli_real_escape_string($conn,$_POST['board_name']);
+					echo 'window.location.href ="post_page.php?board_name='.$board_name.'";';
+				}
+				else if(isset($_POST['building_ID'])){
+					$building_ID = mysqli_real_escape_string($conn,$_POST['building_ID']);
+					echo 'window.location.href ="post_page.php?building_ID='.$building_ID.'";';
+				}
 			?>
 			</script>
 		<?php
