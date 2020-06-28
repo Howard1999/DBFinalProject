e06c10810b9d1f70e8206595bcb60bb77f564176
Howard <a82823553@gmail.com>
2020-06-28 11:21:55 +0800

    Now can rename and delete board in board manage

---
+++ delete_board.php
@@ -0,0 +1,53 @@
+<?php
+	session_start();
+	//connect to database
+	$servername = "127.0.0.1";
+	$username = "team1";
+	$password = "DB338HKkvRVOZzb";
+	$dbname = "team1";
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if (!$conn->set_charset("utf8")) {
+		printf("Error loading character set utf8: %s\n", $conn->error);
+		exit();
+	}
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	// check user authority
+	$is_admin = false;
+	if(isset($_SESSION['session_id'])){
+		$result = $conn->query('select user_name from user where session_id="'.$_SESSION['session_id'].'" and user_authority = "A"');
+		if($row = $result->fetch_row()){
+			$user_name = $row[0];
+			$is_admin = true;
+		}
+		$result->close();
+	}
+	if(!$is_admin){
+		header("Location: /DBFinalProject/index.php");
+		die();
+	}
+	
+	// check post data
+	if(isset($_POST['board_name'])){
+		$board_name = mysqli_real_escape_string($conn,$_POST['board_name']);
+		$result = $conn->query('select * from board where board_name="'.$board_name.'"');
+		if(!$result->fetch_row()){
+			header("Location: /DBFinalProject/board_manage.php?err_msg=board_not_qxist");
+			die();
+		}
+		$result->close();
+	}
+	// delete 
+	if($conn->query('delete from board where board_name="'.$board_name.'"')){
+		header("Location: /DBFinalProject/board_manage.php");
+		die();
+	}
+	else{
+		header("Location: /DBFinalProject/board_manage.php?err_msg=unknown");
+		die();
+	}
+?>
---
+++ rename_board.php
@@ -0,0 +1,66 @@
+<?php
+	session_start();
+	//connect to database
+	$servername = "127.0.0.1";
+	$username = "team1";
+	$password = "DB338HKkvRVOZzb";
+	$dbname = "team1";
+	
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	if (!$conn->set_charset("utf8")) {
+		printf("Error loading character set utf8: %s\n", $conn->error);
+		exit();
+	}
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	// check user authority
+	$is_admin = false;
+	if(isset($_SESSION['session_id'])){
+		$result = $conn->query('select user_name from user where session_id="'.$_SESSION['session_id'].'" and user_authority = "A"');
+		if($row = $result->fetch_row()){
+			$user_name = $row[0];
+			$is_admin = true;
+		}
+		$result->close();
+	}
+	if(!$is_admin){
+		header("Location: /DBFinalProject/index.php");
+		die();
+	}
+	
+	// check post data
+	if(isset($_POST['old_board_name'])&&isset($_POST['new_board_name'])){
+		$old_board_name = mysqli_real_escape_string($conn,$_POST['old_board_name']);
+		$new_board_name = mysqli_real_escape_string($conn,$_POST['new_board_name']);
+		$result = $conn->query('select * from board where board_name="'.$old_board_name.'"');
+		$result2 = $conn->query('select * from board where board_name="'.$new_board_name.'"');
+		if($result2->fetch_row()){
+			header("Location: /DBFinalProject/board_manage.php?err_msg=name_qxist");
+			die();
+		}
+		else if($result->fetch_row()){
+			if(mb_strlen($new_board_name)==0){
+				header("Location: /DBFinalProject/board_manage.php?err_msg=name_empty");
+				die();
+			}
+			else if(mb_strlen($new_board_name)>20){
+				header("Location: /DBFinalProject/board_manage.php?err_msg=name_too_long");
+				die();
+			}
+		}
+		
+		$result->close();
+	}
+	// update 
+	if($conn->query('update board set board_name="'.$new_board_name.'" where board_name="'.$old_board_name.'"')){
+		header("Location: /DBFinalProject/board_manage.php");
+		die();
+	}
+	else{
+		header("Location: /DBFinalProject/board_manage.php?err_msg=unknown");
+		die();
+	}
+?>
